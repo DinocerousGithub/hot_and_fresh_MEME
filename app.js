9b1af60671d3ad1569c73cc13af287d5294d79e3
Jeevan Hastings <jeevanhastings@gmail.com>
2018-08-01 16:29:16 -0700

    added js css editing,added table

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  console.log('app.js sanity check');
+
+  $('.jumbotron').click(function(){
+    $(this).css('background-color', 'rgb(126, 94, 158)');
+  });
+
+
+
+  });
