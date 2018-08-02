7d8b21d1c59195297d4d526a1dbe7f4800a981b5
Jeevan Hastings <jeevanhastings@gmail.com>
2018-08-02 11:04:05 -0700

    jumbotron rng

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ $(document).ready(function(){
   console.log('app.js sanity check');
 
   $('.jumbotron').click(function(){
-    $(this).css('background-color', 'rgb(126, 94, 158)');
-  });
+    var colorArray = ["rgb(0 ,246, 255)", "rgb(25,214,25)", "rgb(178 ,98, 162)", "rgb(221,107,0)", "rgb(18,237,149)"];
+
+    var rng = Math.random();
+    rng = rng * 10 / 2;
+    rng = Math.round(rng)
+
+    console.log(rng)
+    $(this).css('background-color', colorArray[rng] );
+  }); // NOTE: closes jumbotron rng
 
 
 
-  });
+
+
+
+
+
+
+}); // NOTE: closes document.ready()
